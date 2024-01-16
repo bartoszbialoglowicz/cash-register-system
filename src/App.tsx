7f32d2e4fd59013e3b2589a77334f17a43ac3e96
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-01-16 23:48:00 +0100

    feat: Render content base on app state

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LoginForm from './components/Auth/LoginForm';
+import { useContext, useState } from 'react';
+import { UserContext } from './store/user-context';
+import AppLayout from './components/AppLayout/AppLayout';
 
 function App() {
+
+  const userCtx = useContext(UserContext);
+  const content = userCtx.isAuthenticated ? <AppLayout /> : <LoginForm />
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {content}
     </div>
   );
 }
