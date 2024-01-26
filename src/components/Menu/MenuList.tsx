c2ef680449a5326938828722ec10e874e568fbc5
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2024-01-26 22:18:19 +0100

    feat: Added onClick event handler

--- src/components/Menu/MenuList.tsx
+++ src/components/Menu/MenuList.tsx
@@ -4,13 +4,20 @@ import products from '../../assets/svg/shopping-cart.svg';
 import settings from '../../assets/svg/settings.svg';
 import sales from '../../assets/svg/percentage.svg';
 
-const MenuList = () => {
+import './MenuList.css';
+import ProductsController from "../Products/ProductsController";
+
+type Props = {
+    setContentHandler: (element: JSX.Element) => void
+}
+
+const MenuList: React.FC<Props> = (props) => {
 
     const DUMMY_MENU_ITEMS: MenuItem[] = [
         {
             id: 0,
             text: "PRODUKTY",
-            elementToRender: <p></p>,
+            elementToRender: <ProductsController />,
             icon: products,
             requireAdmin: false,
         },
@@ -22,7 +29,7 @@ const MenuList = () => {
             requireAdmin: false
         },
         {
-            id: 0,
+            id: 2,
             text: "KONFIGURACJA",
             icon: settings,
             elementToRender: <p></p>,
@@ -30,16 +37,26 @@ const MenuList = () => {
         },
     ];
 
+    // Change the item rendered in Main component
+    const setItemContentHandler = (item: MenuItem) => {
+        props.setContentHandler(item.elementToRender);
+    }
+
     const getItem = (item: MenuItem) => {
-        return <div className="menu-list-item">
+        return <div className="menu-list-item" onClick={() => setItemContentHandler(item)} key={item.id}>
             <div className="menu-list-item-icon">
                 <img src={item.icon} alt={item.text} />
+            </div>
+            <div className="menu-list-item-text">
+                {item.text}
             </div>
         </div>
     }
 
+    const elements = DUMMY_MENU_ITEMS.map(el => getItem(el));
+
     return <div className="menu-list">
-        
+        {elements}
     </div>
 };
 
