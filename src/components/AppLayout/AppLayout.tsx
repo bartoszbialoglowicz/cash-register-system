ec8c4c6d40339ccbb8444edc8b1cf459f910df28
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2024-01-26 22:07:26 +0100

    feat: Added content state managing

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import MainController from "../Main/MainController";
 import MenuController from "../Menu/MenuController";
 import SummaryPanelController from "../SummaryPanel/SummaryPanelController";
 
 import './AppLayout.css';
+import ProductsController from "../Products/ProductsController";
 
 const AppLayout = () => {
+
+    const [currentContent, setCurrentContent] = useState<JSX.Element>(<ProductsController />);
+
+    const setCurrentContentHandler = (element: JSX.Element) => {
+        setCurrentContent(element);
+    };
+
     return <div className="layout">
-        <MenuController />
-        <MainController />
+        <MenuController setContentHandler={setCurrentContentHandler}/>
+        <MainController content={currentContent}/>
         <SummaryPanelController />
     </div>
 };
