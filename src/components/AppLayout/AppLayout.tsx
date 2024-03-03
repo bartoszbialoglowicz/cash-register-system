33579706add1852cc64b70613ed448d2d3f600a4
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2024-03-03 12:56:54 +0100

    feat: Added admin component render

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,22 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MainController from "../Main/MainController";
 import MenuController from "../Menu/MenuController";
 import SummaryPanelController from "../SummaryPanel/SummaryPanelController";
 
 import './AppLayout.css';
 import ProductsController from "../Products/ProductsController";
+import { MenuItem } from "../../utils/types";
+import AdminWindow from "../AdminWindow/AdminWindows";
+import { AppConfig } from "../../utils/config";
 
 const AppLayout = () => {
 
-    const [currentContent, setCurrentContent] = useState<JSX.Element>(<ProductsController />);
+    const [currentContent, setCurrentContent] = useState<MenuItem>(AppConfig.DEFAULT_MAIN_CONTENT);
+    const [adminAuthenticated, setAdminAuthenticated] = useState(false);
 
-    const setCurrentContentHandler = (element: JSX.Element) => {
-        setCurrentContent(element);
+
+    const authenticationHandler = (password: string) => {
+        setAdminAuthenticated(AppConfig.authenticateAdmin(password));
     };
+
+    const setCurrentContentHandler = (item: MenuItem) => {
+        setCurrentContent(item);
+    };
+
+    const contentToRender = (!adminAuthenticated && currentContent.requireAdmin) ? <AdminWindow authenticationHandler={authenticationHandler}/> : currentContent.elementToRender;
 
     return <div className="layout">
         <MenuController setContentHandler={setCurrentContentHandler}/>
-        <MainController content={currentContent}/>
+        <MainController content={contentToRender}/>
         <SummaryPanelController />
     </div>
 };
