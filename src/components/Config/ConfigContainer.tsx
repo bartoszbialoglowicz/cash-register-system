1a14f40a98c40684743d8e1447630c9fb7e750cd
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2024-03-03 16:31:04 +0100

    feat: Added config content

--- src/components/Config/ConfigContainer.tsx
+++ src/components/Config/ConfigContainer.tsx
@@ -1,6 +1,24 @@
+import ConfigLabel from "./ConfigLabel";
+import ConfigLabelContainer from "./ConfigLabelContainer";
+
 const ConfigContainer = () => {
     return <div className="config">
-        KONFIGURACJA
+        <h1>KONFIGURACJA</h1>
+        <ConfigLabelContainer name="SIEĆ">
+            <ConfigLabel labelText="Adres IP komputera" inputText="10.23.23.21" status="CONNECTED"/>
+        </ConfigLabelContainer>
+        <ConfigLabelContainer name="DRUKARKA FISKALNA">
+            <>
+            <ConfigLabel labelText="Port" inputText="COM" status="CONNECTED" />
+            <ConfigLabel labelText="Adres IP drukarki" inputText="10.23.23.121" />
+            </>
+        </ConfigLabelContainer>
+        <ConfigLabelContainer name="PŁATNOŚCI BEZGOTÓWKOWE">
+            <>
+                <ConfigLabel labelText="Adres IP terminala" inputText="10.23.23.31" status="CONNECTED" />
+                <ConfigLabel labelText="Operator" inputText="Pekao" status="CONNECTED" />
+            </>
+        </ConfigLabelContainer>
     </div>
 };
 
