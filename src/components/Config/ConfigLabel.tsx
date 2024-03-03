4f964b823e13a70826295fb7915d1864d826447d
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2024-03-03 16:31:25 +0100

    feat: Added label input

--- src/components/Config/ConfigLabel.tsx
+++ src/components/Config/ConfigLabel.tsx
@@ -1,5 +1,19 @@
-const ConfigLabel = () => {
+import ConfigLabelStatus from "./ConfigLabelStatus";
 
+type Props = {
+    labelText: string,
+    inputText: string,
+    status?: "CONNECTED" | "DISCONNECTED"
+}
+
+const ConfigLabel: React.FC<Props> = (props) => {
+    return <div className="config-label">
+        <p>{props.labelText}</p>
+        <div className="config-label-input">
+            <input type="text" value={props.inputText} disabled />
+        </div>
+        {props.status && <ConfigLabelStatus status={props.status}/>}
+    </div>
 };
 
 export default ConfigLabel;
