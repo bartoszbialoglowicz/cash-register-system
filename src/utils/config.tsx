57a7bc39089aaca70bcc5253267b89c036b48606
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-04-24 21:29:35 +0200

    feat: Added server url const

--- src/utils/config.tsx
+++ src/utils/config.tsx
@@ -2,10 +2,6 @@ import ProductsController from "../components/Products/ProductsController";
 import { MenuItem } from "./types";
 
 export class AppConfig {
-    // Admin password for testing the app
-    // Credentials will be stored on the server side
-    private static readonly DUMMY_ADMIN_PASSWORD = 'test';
-
     public static readonly DEFAULT_MAIN_CONTENT: MenuItem = {
         id: 0,
         text: "PRODUKTY",
@@ -14,7 +10,5 @@ export class AppConfig {
         requireAdmin: false,
     }
 
-    public static authenticateAdmin = (password: string) => {
-        return password === this.DUMMY_ADMIN_PASSWORD;
-    }
+    public static SERVER_URL = '127.0.0.1:8000';
 }
