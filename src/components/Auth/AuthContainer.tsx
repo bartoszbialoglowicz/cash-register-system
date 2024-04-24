050e837917fb2f30389514781ad7cde0582c0fb5
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-04-24 21:31:08 +0200

    feat: Sending post method on form submit

--- src/components/Auth/AuthContainer.tsx
+++ src/components/Auth/AuthContainer.tsx
@@ -1,54 +1,53 @@
-import { FormEvent, useContext, useRef, useState } from "react";
-import { ServerAuthResponse, User } from "../../utils/types";
+import { FormEvent, useContext, useEffect, useRef, useState } from "react";
 import { UserContext } from "../../store/user-context";
 
 import './AuthContainer.css';
 import LoginForm from "./LoginForm";
+import { TokenResponse } from "../../utils/types";
+import { TOKEN_URL } from "../../utils/urls";
+import { usePublicApiRequest } from "../../hooks/use-public-api-request";
 
 const AuthContainer: React.FC = () => {
 
     const inputValue = useRef<HTMLInputElement>(null);
     const passwordValue = useRef<HTMLInputElement>(null);
 
-    const [error, setError] = useState<string>('');
-
-    const DUMMY_USERNAME_CHECK = "test";
-    const DUMMY_PASSWORD_CHECK = "test";
-
     const userCtx = useContext(UserContext);
+    const {isLoading, error, sendRequest} = usePublicApiRequest();
 
-    const dummy_check = () => {
-        let isValid = true;
-        if (inputValue.current!.value !== DUMMY_USERNAME_CHECK)
-            isValid = false;
-        if (passwordValue.current!.value !== DUMMY_PASSWORD_CHECK)
-            isValid = false;
-        const response: ServerAuthResponse  = isValid ? {code: 200, message: "Succesfully logged in", user: {id: 1, username: "test", isAdmin: false}} : {code: 401, message: "Invalid credentials"};
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
+    const getResponse = async () => {
+        const response = await sendRequest<TokenResponse>({method: "POST", url: TOKEN_URL, body:{username: inputValue.current!.value, password: passwordValue.current!.value}});
         return response;
     }
 
-    const getResponse = async () => {
-        const {code, message, user} = await dummy_check();
-        return {code, message, user};
-    }
-
-    const handleResponse = async () => {
-        const {code, message, user} = await getResponse();
-        if (code === 200 && user) {
-            userCtx.login(user);
-            return;
+    const handleResponse = async (response: TokenResponse | undefined) => {
+        if (response) {
+            console.log(response);
+        } else if (error) {
+            setErrorMessage("Error");
         }
-        setError(message);
     }
 
-    const formSubmitHandler = (event: FormEvent) => {
+    const formSubmitHandler = async (event: FormEvent) => {
         event.preventDefault();
-        handleResponse();
+        const response = await getResponse();
+        handleResponse(response);
     }
+
+    useEffect(() => {
+        setErrorMessage(null);
+        if (error && error.code === 401) {
+            setErrorMessage("Invalid credentials!");
+        } else if (error){
+            setErrorMessage(error.message);
+        }
+    }, [error])
 
     return <div className="auth">
-        <LoginForm formSubmitHandler={formSubmitHandler} inputValue={inputValue} passwordValue={passwordValue} error={error}/>
+        <LoginForm formSubmitHandler={formSubmitHandler} inputValue={inputValue} passwordValue={passwordValue} error=""/>
+        {errorMessage && errorMessage}
     </div>
 };
 
