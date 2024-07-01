ff180eff43fbce72b75266429dea85a28ea2ffb8
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-07-02 00:30:05 +0200

    feat: Added text label

--- src/components/Products/ProductsSearchBar.tsx
+++ src/components/Products/ProductsSearchBar.tsx
@@ -11,6 +11,7 @@ const ProductsSearchBar: React.FC<Props> = (props) => {
 const inputRef = useRef<HTMLInputElement>(null);
 
     return <div className="products-search-bar">
+        <label>Filtruj po nazwie</label>
         <input type="text" placeholder="Nazwa produktu..." ref={inputRef} onChange={() => props.onChange(inputRef.current!.value)}/>
     </div>
 };
