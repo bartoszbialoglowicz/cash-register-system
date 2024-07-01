6d245d25aba65a34c0b0748146e8c2c97b8ca6c0
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-07-02 00:21:43 +0200

    feat: Added loading text information

--- src/components/LoadingScreen/LoadingScreen.tsx
+++ src/components/LoadingScreen/LoadingScreen.tsx
@@ -5,6 +5,7 @@ import './LoadingScreen.css';
 const LoadingScreen = () => {
     return <div className="loading-screen">
         <img src={loading} alt='loading' />
+        <p>Trwa wczytywanie danych...</p>
     </div>
 };
 
