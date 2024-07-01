51713b9d60ee3dd74360b3ea2c7abf977529b9ef
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-07-02 00:23:58 +0200

    feat: Added check icon

--- src/components/Products/ProductCategoryItem.tsx
+++ src/components/Products/ProductCategoryItem.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
 import { ProductCategory } from '../../utils/types';
+import checked from '../../assets/svg/checked-tick-svgrepo-com.svg';
+
 import './ProductCategoryItem.css';
 
 type Props = {
@@ -7,8 +10,23 @@ type Props = {
 }
 
 const ProductCategoryItem: React.FC<Props> = (props) => {
-    return <div className="product-cateogory-item" onClick={() => props.onClick(props.name)}>
-        {props.name}
+
+    const [isChecked, setIsChecked] = useState(false);
+
+    const onClickHandler = () => {
+        setIsChecked(prevState => !prevState);
+        props.onClick(props.name);
+    }
+
+    const className = isChecked ? 'checked' : '';
+
+    return <div className="product-cateogory-item" onClick={onClickHandler}>
+        <div className={`product-category-item-checkbox ${className}`}>
+            {isChecked && <img src={checked} alt='checked' />}
+        </div>
+        <div className='product-category-item-name'>
+            <p>{props.name}</p>
+        </div>
     </div>
 };
 
