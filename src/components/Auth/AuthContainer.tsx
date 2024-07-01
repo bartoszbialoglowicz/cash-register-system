9288e23113636f7f749471f4e8d3cb0d72983292
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-07-02 00:20:00 +0200

    feat: Added user login handler. Added error feedback

--- src/components/Auth/AuthContainer.tsx
+++ src/components/Auth/AuthContainer.tsx
@@ -3,9 +3,11 @@ import { UserContext } from "../../store/user-context";
 
 import './AuthContainer.css';
 import LoginForm from "./LoginForm";
-import { TokenResponse } from "../../utils/types";
-import { TOKEN_URL } from "../../utils/urls";
+import { TokenResponse, User } from "../../utils/types";
+import { TOKEN_URL, USER_URL } from "../../utils/urls";
 import { usePublicApiRequest } from "../../hooks/use-public-api-request";
+import ErrorAlert from "../UI/ErrorAlert";
+import { useApiRequest } from "../../hooks/use-api-request";
 
 const AuthContainer: React.FC = () => {
 
@@ -14,8 +16,10 @@ const AuthContainer: React.FC = () => {
 
     const userCtx = useContext(UserContext);
     const {isLoading, error, sendRequest} = usePublicApiRequest();
+    const {isLoading: isLoading2, error: error2, sendRequest: sendPrivateRequest} = useApiRequest();
 
     const [errorMessage, setErrorMessage] = useState<string | null>(null);
+    const [isErrorAlertVisible, setIsErrorAlertVisible] = useState(false);
 
     const getResponse = async () => {
         const response = await sendRequest<TokenResponse>({method: "POST", url: TOKEN_URL, body:{username: inputValue.current!.value, password: passwordValue.current!.value}});
@@ -24,30 +28,47 @@ const AuthContainer: React.FC = () => {
 
     const handleResponse = async (response: TokenResponse | undefined) => {
         if (response) {
-            console.log(response);
+            const user = await sendPrivateRequest<User>({method: "GET", url: USER_URL, body: undefined, authoritzation: response.access});
+            if (user) {
+                userCtx.login(user, response);
+            }
         } else if (error) {
-            setErrorMessage("Error");
+            setErrorMessage(error.message);
         }
+    }
+
+    const showErrorMessageHandler = (message: string) => {
+        setErrorMessage(message);            
+        setIsErrorAlertVisible(true);
     }
 
     const formSubmitHandler = async (event: FormEvent) => {
         event.preventDefault();
-        const response = await getResponse();
-        handleResponse(response);
+        if (inputValue.current!.value === '' || passwordValue.current!.value === '') {
+            showErrorMessageHandler("Wypełnij wszystkie pola!");
+        }
+        else { 
+            const response = await getResponse();
+            handleResponse(response);
+        }
     }
 
     useEffect(() => {
         setErrorMessage(null);
         if (error && error.code === 401) {
-            setErrorMessage("Invalid credentials!");
+            showErrorMessageHandler("Invalid credentials");
         } else if (error){
-            setErrorMessage(error.message);
+            showErrorMessageHandler(error.message);
         }
     }, [error])
 
+    const hideAlertHandler = () => {
+        setIsErrorAlertVisible(false);
+    }
+
     return <div className="auth">
         <LoginForm formSubmitHandler={formSubmitHandler} inputValue={inputValue} passwordValue={passwordValue} error=""/>
-        {errorMessage && errorMessage}
+        {(isErrorAlertVisible && errorMessage) && <ErrorAlert errorMessage={errorMessage} onButtonClick={hideAlertHandler} buttonText={"OK"}/>}
     </div>
 };
 
