639eda30aee3718e70a71238de58c8db476e3401
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-07-02 00:23:01 +0200

    refactor: Removed test component

--- src/components/Menu/MenuController.tsx
+++ src/components/Menu/MenuController.tsx
@@ -1,7 +1,6 @@
 import { MenuItem } from '../../utils/types';
 import './MenuController.css';
 import MenuList from './MenuList';
-import MenuUserPanel from './MenuUserPanel';
 
 type Props = {
     setContentHandler: (item: MenuItem) => void
@@ -9,7 +8,6 @@ type Props = {
 
 const MenuController: React.FC<Props> = (props) => {
     return <div className="menu">
-        <MenuUserPanel />
         <MenuList setContentHandler={props.setContentHandler}/>
     </div>
 };
