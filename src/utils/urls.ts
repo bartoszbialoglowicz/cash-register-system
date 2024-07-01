3929780688807047d805f5a74ff4b7b7132d2a85
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-07-02 00:35:34 +0200

    feat: Added urls

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,2 +1,5 @@
 export const TOKEN_URL = 'users/token/';
 export const REFRESH_TOKEN_URL = 'users/token/refresh/';
+export const USER_URL = 'users/me/';
+export const CATEGORY_URL = 'api/categories/';
+export const PRODUCTS_URL = 'api/products/';
