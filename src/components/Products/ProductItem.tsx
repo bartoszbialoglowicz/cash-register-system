4ac21ed327482247a908cfeb174bf8b10204b95e
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-07-02 00:25:40 +0200

    feat: Created product item component

---
+++ src/components/Products/ProductItem.tsx
@@ -0,0 +1,9 @@
+import { Product } from "../../utils/types";
+
+const ProductItem: React.FC<{product: Product}> = (props) => {
+    return <div className="product-item">
+        
+    </div>
+};
+
+export default ProductItem;
