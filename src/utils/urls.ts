f84d7f69cd8fd79ae5bf43c56524c29ab31b81c7
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-07-28 14:15:27 +0200

    feat: Added manufacturer, discount routes

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -3,3 +3,5 @@ export const REFRESH_TOKEN_URL = 'users/token/refresh/';
 export const USER_URL = 'users/me/';
 export const CATEGORY_URL = 'api/categories/';
 export const PRODUCTS_URL = 'api/products/';
+export const MANUFACTURERS_LIST_URL = 'api/manufacturers/';
+export const DISCOUNTS_LIST_URL = 'api/discounts/';
