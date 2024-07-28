d28c1b9e08a595d8733f2969d813cb7e1bcd6072
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-07-28 14:13:53 +0200

    feat: Added manufacturer filter

--- src/components/Products/ProductsController.tsx
+++ src/components/Products/ProductsController.tsx
@@ -1,4 +1,4 @@
-import { Product, ProductCategory } from "../../utils/types";
+import { Manufacturer, Product, ProductCategory } from "../../utils/types";
 import LoadingScreen from "../LoadingScreen/LoadingScreen";
 import ProductsSearchBar from "./ProductsSearchBar";
 
@@ -11,28 +11,32 @@ import { UserContext } from "../../store/user-context";
 import ErrorCard from "../UI/ErrorCard";
 import ProductsTable from "./ProductsTable";
 import ProductsPriceFilter from "./ProductsPriceFilter";
+import ProductsManufacturerFilter from "./ProductsManufacturerFilters";
+import { AlertContext } from "../../store/alert-context";
 
 type Filter = {
     minPrice: number | null,
     maxPrice: number | null,
     name: string | null,
-    category: ProductCategory[],
+    category: string[],
+    manufacturer: string[],
 }
 
 const ProductsController = () => {
 
     const [allProducts, setAllProducts] = useState<Product[]>([]);
-    const [currentCategories, setCurrentCategories] = useState<ProductCategory[]>([]);
     const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
     const [currentFilters, setCurrentFilters] = useState<Filter>({
         minPrice: null,
         maxPrice: null,
         name: null,
-        category: []
+        category: [],
+        manufacturer: [],
     });
 
     const {isLoading, error, sendRequest} = useApiRequest();
     const userCtx = useContext(UserContext);
+    const alertCtx = useContext(AlertContext);
 
     const onSearchBarChangeHandler = (text: string | null) => {
         setCurrentFilters(prevState => ({...prevState, name: text}));
@@ -46,7 +50,7 @@ const ProductsController = () => {
         setCurrentFilters(prevState => ({...prevState, maxPrice: value}));
     };
 
-    const onClickCategoryHandler = (category: ProductCategory) => {
+    const onClickCategoryHandler = (category: string) => {
         setCurrentFilters(prevState=> {
             const index = prevState.category.findIndex(el => el === category);
             if (index === -1)
@@ -56,12 +60,26 @@ const ProductsController = () => {
         });
     };
 
+    const onClickManufacturerHandler = (manufacturer: string) => {
+        setCurrentFilters(prevState=> {
+            const index = prevState.manufacturer.findIndex(el => el === manufacturer);
+            if (index === -1)
+                return {...prevState, manufacturer: [...prevState.manufacturer, manufacturer]};
+            else
+                return {...prevState, manufacturer: prevState.manufacturer.filter(el => el !== manufacturer)}
+        });
+    };
+
     const filterProductByName = (text: string, product: Product) => {
         return product.name.toLowerCase().includes(text.toLowerCase());
     };
 
-    const filterProductByCategory = (categories: ProductCategory[], product: Product) => {
+    const filterProductByCategory = (categories: string[], product: Product) => {
         return categories.find(el => el === product.category);
+    }
+
+    const filterProductByManfacturer = (manufacturers: string[], product: Product) => {
+        return manufacturers.find(el => el === product.manufacturer.name);
     }
 
     const applyFilters = () => {
@@ -70,7 +88,8 @@ const ProductsController = () => {
                 return (!currentFilters.name || filterProductByName(currentFilters.name, product)) &&
                     (!currentFilters.minPrice || product.price >= currentFilters.minPrice) &&
                     (!currentFilters.maxPrice || product.price <= currentFilters.maxPrice) &&
-                    ((currentFilters.category.length === 0) || filterProductByCategory(currentFilters.category, product));
+                    ((currentFilters.category.length === 0) || filterProductByCategory(currentFilters.category, product)) &&
+                    ((currentFilters.manufacturer.length === 0) || filterProductByManfacturer(currentFilters.manufacturer, product))
             })
         });
     };
@@ -96,11 +115,18 @@ const ProductsController = () => {
         getData();
     }, []);
 
+    useEffect(() => {
+        if (error) {
+            alertCtx?.addAlert('Nie udało się pobrać listy produktów', 'error');
+        }
+    }, [error]);
+
     
     return <div className="products">
         <div className="products-filter-panel">  
             <ProductsSearchBar  onChange={onSearchBarChangeHandler}/>
             <ProductsCategoryFilter onClick={onClickCategoryHandler}/>
+            <ProductsManufacturerFilter onClick={onClickManufacturerHandler}/>
             <ProductsPriceFilter onMinPriceChange={onMinPriceChangeHandler} onMaxPriceChange={onMaxPriceChangeHandler}/>
         </div>
         <div className="products-items">
