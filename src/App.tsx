f59437e99da9f86129ccc82fcbfbc7d29b90a745
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-07-28 14:09:33 +0200

    feat: Created routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,32 @@ import AuthContainer from './components/Auth/AuthContainer';
 import { useContext, useState } from 'react';
 import { UserContext } from './store/user-context';
 import AppLayout from './components/AppLayout/AppLayout';
+import { AlertProvider } from './store/alert-context';
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
+import ProtectedRoute from './components/Router/ProtectedRoute';
 
 function App() {
 
   const userCtx = useContext(UserContext);
-  const content = userCtx.isAuthenticated ? <AppLayout /> : <AuthContainer />
+  const router = createBrowserRouter([
+    {
+      path: "auth",
+      element: <AuthContainer />
+    },
+    {
+      path: "/",
+      element: <ProtectedRoute isAuthenticated={userCtx.isAuthenticated}><AppLayout /></ProtectedRoute>
+    },
+    {
+      path: "*",
+      element: <Navigate to={userCtx.isAuthenticated ? "/" : "auth"} />
+    }
+  ])
 
   return (
-    <div className="App">
-      {content}
-    </div>
+    <AlertProvider>
+      <RouterProvider router={router} />
+    </AlertProvider>
   );
 }
 
